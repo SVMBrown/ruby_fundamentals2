46ae2f22767ffc6806338bd1f5dbe6c14a076a89
SVMBrown <scotb38@gmail.com>
2015-04-01 15:41:40 -0400

    Do I need bananas??

--- exercise6.rb
+++ exercise6.rb
@@ -7,3 +7,4 @@ grocery_list << "rice"
 puts "Almost forgot my rice!!"
 print_my(grocery_list)
 puts "I need to get #{grocery_list.length} items."
+puts "I #{grocery_list.index("bananas") ? "" : "don't "}need to pick up bananas today."
